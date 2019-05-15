e6254273a2e78aaebe2a5adf7d814be109a27ae5
Adriano <adrianomrodrigues93@gmail.com>
2019-05-15 15:24:54 -0300

    adding sort function array example

---
+++ order/index.js
@@ -0,0 +1,16 @@
+const initArr = [{0: {v: 'teste'}, 1: {v: 'teste'}},{0: {v: 'teste3'}, 1: {v: 'teste3'}},{0: {v: 'teste2'}, 1: {v: 'teste2'}}]
+var arr = initArr.slice(0)
+console.log(initArr)
+
+arr = arr.sort((a,b) => {
+    if (a[0].v > b[0].v) {
+        return 1;
+      }
+      if (a[0].v < b[0].v) {
+        return -1;
+      }
+      // a must be equal to b
+      return 0;
+})
+
+console.log(arr)
